3aae7895ce0e2b201e227630fd66cbae3fe23574
Miguel Gutierrez <miguel.gutierrez@aa.com>
2020-10-22 12:30:13 -0700

    added internal video list

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,10 @@ import * as functions from 'firebase-functions';
 const express = require('express');
 const cors = require('cors'); // CORS Express middleware to enable CORS Requests.
 const axios = require('axios')
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+const db = admin.firestore();
 
 const app = express();
 app.use(cors());
@@ -57,7 +61,64 @@ app.get('/getYoutubeVideos', async (req: functions.https.Request, res: functions
     });
 });
 
+app.get('/getVideoList', async (req: functions.https.Request, res: functions.Response) => {
+    const getVideoList = () => { 
+        return new Promise((resolve, reject) => {
+            db.collection('content').get().then((snapshot: any) => {
+                const dat: any = [];
+                snapshot.forEach((doc: any) => {
+                    dat.push(doc.data());
+                    console.log(doc.id, '=>', doc.data());
+                });
+                resolve(dat)
+            });
+        });
+    };
+    return getVideoList().then((val) => res.send(val) );
+});
 
 
+app.get('/videos', async (req: functions.https.Request, res: functions.Response) => {
+    return db.collection('content').doc('videos').get().then((videos: any) => {
+        res.send(videos.data().list);
+    });
+});
+
+
+app.post('/videos', async (req: functions.https.Request, res: functions.Response) => {
+    const video = req.body;
+    const videosRef = db.collection('content').doc('videos');
+    return videosRef.get().then((vals: any) => {
+        const videos = vals.data();
+        if (!videos.list) videos.list = [];
+    
+        // validate object: thumbnails, order, link, description, sauce
+        if (video) videos.list.push(video);
+        console.log(videos)
+    
+        return videosRef.update(videos).then(() => res.send('success'));
+    });
+});
+
+app.delete('/videos', async (req: functions.https.Request, res: functions.Response) => {
+    if (!req || !req.body || !req.body.id) {
+        res.status(400).send('ID was not passed in body');
+        return;
+    }
+    const id = req.body.id;
+    const videosRef = db.collection('content').doc('videos');
+    return videosRef.get().then((vals: any) => {
+        const videos = vals.data();
+        if (!videos.list) videos.list = [];
+
+        const newList: any = []
+        videos.list.map((v: any) => {
+            if (v.id !== id) newList.push(v);
+        });
+        videos.list = newList;
+    
+        return videosRef.update(videos).then(() => res.send('success'));
+    });
+});
 
 exports.api = functions.https.onRequest(app);
